836606d51555bcb923150ed1e3f0888d6688e853
xleMnlx <eman.darca86@gmail.com>
2023-06-14 11:42:20 +0800

    Add project files.

---
+++ QRVidFeed/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace QRVidFeed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 229);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(229, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(27, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(229, 168);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ QRVidFeed/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using TouchlessLib;
+using System.Diagnostics;
+using ZXing;
+using System.Data.SqlClient;
+
+namespace QRVidFeed
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("data source = localhost; database = BevPizza ; integrated security = true;");
+        TouchlessMgr touch = new TouchlessMgr();
+        BarcodeReader barcodeReader = new BarcodeReader();
+        Result result;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void initializeCam()
+        {
+            touch.CurrentCamera = touch.Cameras[0];
+            touch.CurrentCamera.CaptureWidth = 500; // Set width
+            touch.CurrentCamera.CaptureWidth = 500; // Set height
+            touch.CurrentCamera.OnImageCaptured += new EventHandler<CameraEventArgs>(OnImageCaptured); // Set preview callback function
+        }
+
+        private void OnImageCaptured(object sender, CameraEventArgs args)
+        {
+            // Get the bitmap
+            Bitmap bitmap = args.Image;
+
+            // Read barcode and show results in UI thread
+            this.Invoke((MethodInvoker)delegate
+            {
+                pictureBox1.Image = bitmap;
+                ReadBarcode(bitmap);
+            });
+        }
+
+        private void ReadBarcode(Bitmap bitmap)
+        {
+            // Read barcodes with Dynamsoft Barcode Reader
+            Stopwatch sw = Stopwatch.StartNew();
+            sw.Start();
+            result = barcodeReader.Decode(bitmap);
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed.TotalMilliseconds + "ms");
+
+            // Clear previous results
+            //textBox1.Clear();
+
+            if (result == null)
+            {
+                textBox1.Text = "No barcode detected!";
+                return;
+            }
+
+            textBox1.Text = "scanned";
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM order_archive WHERE qr_code=@qrBin", con);
+            cmd.Parameters.AddWithValue("@qrBin", result.ToString());
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    string value = reader["qr_code"].ToString();
+                    if (value.Equals(result.ToString()))
+                    {
+                        MessageBox.Show("served!");
+                        //dto ka magdagdag ng info na ipprint
+                        return;
+                    }
+                }
+            }
+            con.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initializeCam();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.ExitThread();
+        }
+    }
+}
